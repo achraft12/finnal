2605b9242d7dc7c05b5b19edd8e7835d8c80bfbe
achraft12 <achrafachraftaki@gmail.com>
2024-11-18 23:06:53 +0100

    Initial commit: frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 
-
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
 
@@ -411,9 +411,12 @@ app.delete('/events/:id', (req, res) => {
   });
 });
 
-
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
