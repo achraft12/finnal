078ad91ec563c09c2c1a5e0b41e227d741539d4f
achraft12 <achrafachraftaki@gmail.com>
2024-11-19 01:02:32 +0100

    commiting all

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -29,6 +29,10 @@ const routes = [
     component: ProfilePage,
   },
   {
+    path: '/',
+    redirect:'/bookPage'
+  },
+  {
     path: '/admin/add-book',
     name: 'AdminAddBook',
     component: AdminAddBook,
